f1f01c41834c25a133f44f260f8de4046a79fcc3
Anita-ihuman <charlesanita403@gmail.com>
2021-03-14 10:07:23 +0100

    Creating styles for the footer and nav

---
+++ pages/api/hello.tsx
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// interface ElementProps {
+//   res: string & number
+//  status: never
+// }
+//  export function Element({ res }: ElementProps) {
+//    res.status(200).json({ name: 'John Doe' })
+//  }
+// export default Element
