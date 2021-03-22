2147c8dace665bac8579ca7b186c10268e32fe5a
Anita-ihuman <charlesanita403@gmail.com>
2021-03-22 15:17:23 +0100

    importing images

--- Components/Content/Intro/ImgComponent.tsx
+++ Components/Content/Intro/ImgComponent.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-function Imgcomp(src) {
-  let imgStyles = {
-    width: 100 + "%",
-    height:"auto"
-  }
-  return (
-    <img src={src} alt="slide image" style={imgStyles}/>
-  )
-}
+// import React from 'react'
 
-export default Imgcomp;
+
+// function Imgcomp({src}) {
+//   let imgStyles = {
+//     width: 100 + '%',
+//     height: 'auto',
+//   }
+//   return <img src={src} alt="slide image" style={imgStyles} />
+// }
+
+// export default Imgcomp
