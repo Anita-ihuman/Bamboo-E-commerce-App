3f7d9d5a6ec3703fc558151853c6068d51a6855a
Anita-ihuman <charlesanita403@gmail.com>
2021-03-22 08:51:58 +0100

    Fixed the home

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,2 +1,33 @@
-import styles from './Home.module.css'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
+interface HomeProp {
+  intro: string
+  text: string
+  text2: string
+  text3: string
+  button: string
+  head: string
+}
+
+
+export function Home({ head, intro, text, text3, text2, button }: HomeProp) {
+  return (
+    <>
+      <header className={styles.header}>
+        <article className={styles.content}>
+          <h3>{intro}</h3>
+          <h1>{head}</h1>
+          <p>{text}</p>
+          <p>{text2}</p>
+          <p>{text3}</p>
+          <img src="/Navigation.png" width="80%" />
+          <div>
+            <button className={styles.btn}>{button}</button>
+          </div>
+        </article>
+      </header>
+    </>
+  )
+}
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Home from './Home'
 
-interface HomeProp {
-  intro: string
-  text: string
-  button: string
-  head: string
-}
-
-
-const HomeProp: HomeProp[] = [
-  {
-    intro: 'INDOORS',
-    head: 'See Bamboo’s New Range Of Indoor Furniture',
-    text:
-      'Classic meet modern living Bamboo has a fantastic range of indoor furniture,perfect for any modern home',
-    button: 'SEE MORE',
-  }
-]
-
-export function Home({}: HomeProp) {
+const App = () => {
   return (
-    <>
+    <main className={styles.first}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/Logo2.png" />
       </Head>
-      <header className={styles.header}>
-        {HomeProp.map(({ text, head, intro, button }) => (
-          <article className={styles.content}>
-            <h3>{intro}</h3>
-            <h1>{head}</h1>
-            <p>{text}</p>
-            <img src="/Navigation.png" width="80%" />
-            <div>
-              <button className={styles.btn}>{button}</button>
-            </div>
-          </article>
-        ))}
-      </header>
-    </>
+      <Home
+        intro="INDOORS"
+        head="See Bamboo’s New Range Of Indoor Furniture"
+        text="Classic meet modern living"
+        text2="Bamboo has a fantastic range of indoor furniture,"
+        text3=" perfect for any modern home"
+        button="SEE MORE"
+      />
+    </main>
   )
 }
-export default Home
+
+export default App
