d7ed1f816f559a90951e37ed75726646e41ac815
Anita-ihuman <charlesanita403@gmail.com>
2021-04-02 22:47:29 +0100

    folder and file management for casestufy

---
+++ Components/Content/Casestudy/Casestude.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Link from '../../../node_modules/next/link'
+import styles from './Featured.module.css'
+import Masonry from 'react-masonry-css'
+
+const Casestudy = () => {
+  return (
+    <>
+      <section className={styles.section}>
+        <nav className={styles.nav}>
+          <img className="logo"></img>
+        </nav>
+        <h1 className={styles.boldtext}>goal</h1>
+        <div className={styles.container}>
+          <div className={styles.container_textleft}>
+            <h3>SALE SOLUTION</h3>
+            <p>
+              The Bamboo E-commerce site provide a digital space were digital transaction can take
+              place between the buyer and seller.
+            </p>
+          </div>
+          <br />
+          <div className={styles.container_texright}>
+            <h3>PRODUCT HUB</h3>
+            <p>
+              The Platform provide a single source of truth for all all things furniture to
+              interested parties and eventually be converted to a Lead
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Casestudy
---
+++ Components/Content/Casestudy/goal..tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Link from '../../../node_modules/next/link'
+import styles from './Featured.module.css'
+import Masonry from 'react-masonry-css'
+
+const App = () => {
+  return (
+    <>
+      <section className={styles.section}>
+        <nav className={styles.nav}>
+          <img className="logo"></img>
+        </nav>
+        <h1 className={styles.boldtext}>Casestudy</h1>
+        <div className={styles.container}>
+          <div className={styles.container_text}>
+            <p>
+              Bamboo is a furnitutre company that provide quality product to meet your indoor or
+              outdoor furniture needs.
+            </p>
+            <br />
+            <p>
+              Bamboo e-commerce site is a solution that offer a simple system that organize
+              different product where peoples(families, businesses) can intereact with the product.
+            </p>
+          </div>
+          <div className={styles.container_imgs}>
+            <img src="" />
+            <img src="" />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
